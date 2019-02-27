fc3868067a22b916167c7c7f1dbc59b11a0a1ee0
oOHiyoriOo <wolf.sf75@googlemail.com>
2019-02-27 16:02:00 +0100

    started login mechanism and more design

--- client/Form1.vb
+++ client/Form1.vb
@@ -6,7 +6,24 @@
         path = My.Computer.FileSystem.CurrentDirectory
         Me.BackgroundImage = System.Drawing.Image.FromFile(path + "\DATA\images\diana_background.png")
     End Sub
+
+    Private Sub password_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles password.KeyPress
+        If e.KeyChar = Microsoft.VisualBasic.ChrW(Keys.Return) Then
+            Login_Click(sender, e)
+        End If
+    End Sub
+
     Private Sub Login_Click(sender As Object, e As EventArgs) Handles Login.Click
-        If username.Text <> "" And password.Text <> "" Then MsgBox(username.Text + ":" + password.Text)
+        If username.Text <> "" And password.Text <> "" Then
+            MsgBox("Login")
+        Else
+            If username.Text = "" Then
+                MsgBox("Username is Empty")
+                username.Focus()
+            Else
+                MsgBox("Password is Empty")
+                password.Focus()
+            End If
+        End If
     End Sub
 End Class
