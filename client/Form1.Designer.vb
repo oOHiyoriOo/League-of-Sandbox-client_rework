5cd6f0264be54bd618a5857e331ccd0558e65bc7
oOHiyoriOo <wolf.sf75@googlemail.com>
2019-02-27 14:34:42 +0100

    added base files
    added my projekt files ( yup already started)

---
+++ client/Form1.Designer.vb
@@ -0,0 +1,149 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Lg_Bg = New System.Windows.Forms.GroupBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.username = New System.Windows.Forms.TextBox()
+        Me.password = New System.Windows.Forms.TextBox()
+        Me.Login = New System.Windows.Forms.Button()
+        Me.Lg_Bg.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Lg_Bg
+        '
+        Me.Lg_Bg.AccessibleRole = System.Windows.Forms.AccessibleRole.Outline
+        Me.Lg_Bg.BackColor = System.Drawing.Color.Transparent
+        Me.Lg_Bg.BackgroundImage = CType(resources.GetObject("Lg_Bg.BackgroundImage"), System.Drawing.Image)
+        Me.Lg_Bg.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Lg_Bg.Controls.Add(Me.GroupBox1)
+        Me.Lg_Bg.Controls.Add(Me.Login)
+        Me.Lg_Bg.Location = New System.Drawing.Point(499, -10)
+        Me.Lg_Bg.Name = "Lg_Bg"
+        Me.Lg_Bg.Size = New System.Drawing.Size(324, 740)
+        Me.Lg_Bg.TabIndex = 0
+        Me.Lg_Bg.TabStop = False
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.Transparent
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.username)
+        Me.GroupBox1.Controls.Add(Me.password)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.ForeColor = System.Drawing.Color.BlanchedAlmond
+        Me.GroupBox1.Location = New System.Drawing.Point(0, 211)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(324, 176)
+        Me.GroupBox1.TabIndex = 5
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Login"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(35, 45)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Username"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(37, 116)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Password"
+        '
+        'username
+        '
+        Me.username.Location = New System.Drawing.Point(38, 61)
+        Me.username.Name = "username"
+        Me.username.Size = New System.Drawing.Size(206, 26)
+        Me.username.TabIndex = 1
+        '
+        'password
+        '
+        Me.password.Location = New System.Drawing.Point(38, 132)
+        Me.password.Name = "password"
+        Me.password.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.password.Size = New System.Drawing.Size(206, 26)
+        Me.password.TabIndex = 2
+        '
+        'Login
+        '
+        Me.Login.BackgroundImage = CType(resources.GetObject("Login.BackgroundImage"), System.Drawing.Image)
+        Me.Login.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Login.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Login.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Login.FlatAppearance.BorderSize = 2
+        Me.Login.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Teal
+        Me.Login.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Login.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Login.ForeColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Login.Location = New System.Drawing.Point(6, 393)
+        Me.Login.Name = "Login"
+        Me.Login.Size = New System.Drawing.Size(312, 43)
+        Me.Login.TabIndex = 0
+        Me.Login.Text = "Login"
+        Me.Login.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Desktop
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(1350, 729)
+        Me.Controls.Add(Me.Lg_Bg)
+        Me.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "League of Sandbox Client"
+        Me.Lg_Bg.ResumeLayout(False)
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Lg_Bg As GroupBox
+    Friend WithEvents password As TextBox
+    Friend WithEvents username As TextBox
+    Friend WithEvents Login As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ client/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Dim path As String
+    Dim uname, pw As String
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        path = My.Computer.FileSystem.CurrentDirectory
+        Me.BackgroundImage = System.Drawing.Image.FromFile(path + "\DATA\images\diana_background.png")
+    End Sub
+    Private Sub Login_Click(sender As Object, e As EventArgs) Handles Login.Click
+        If username.Text <> "" And password.Text <> "" Then MsgBox(username.Text + ":" + password.Text)
+    End Sub
+End Class
